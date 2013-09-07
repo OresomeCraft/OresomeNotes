7a0c34b3e42eb7a646b7cb753f3df12672cfbf40
Jordyn Newnham <ttghusky@live.com.au>
2013-09-07 14:04:31 +1000

    Fix outstanding issues involving reading

--- src/com/huskehhh/oresomenotes/OresomeCommands.java
+++ src/com/huskehhh/oresomenotes/OresomeCommands.java
@@ -14,9 +14,9 @@ public class OresomeCommands implements CommandExecutor {
             if (sender.hasPermission("oresomenotes.staff")) {
                 if (args.length <= 16 && args.length >= 0) {
                     String query = args[0];
-                    String perform = args[1];
 
                     if (query.equalsIgnoreCase("add")) {
+                        String perform = args[1];
                         String extra = Utility.argBuilding(args);
                         if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
                             Utility.addNote(OresomeNotes.getInstance().getServer().getPlayer(perform), Utility.prepareForConfig(extra));
@@ -26,6 +26,7 @@ public class OresomeCommands implements CommandExecutor {
                         sender.sendMessage(ChatColor.GREEN + "Successfully added note; " + extra + " to " + ChatColor.RED + perform + ChatColor.GREEN + "'s notes!");
 
                     } else if (query.equalsIgnoreCase("remove")) {
+                        String perform = args[1];
                         if (Integer.valueOf((args[2])) != null) {
                             int entry = Integer.parseInt((args[2]));
                             if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
@@ -39,6 +40,7 @@ public class OresomeCommands implements CommandExecutor {
                         return true;
 
                     } else if (query.equalsIgnoreCase("read")) {
+                        String perform = args[1];
                         if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
                             ListIterator<String> read = Utility.iterRead(OresomeNotes.getInstance().getServer().getPlayer(perform));
                             sender.sendMessage(ChatColor.GREEN + "=====Notes for " + ChatColor.RED + perform + ChatColor.GREEN + "======");
@@ -52,6 +54,19 @@ public class OresomeCommands implements CommandExecutor {
                             sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
                         }
                         return true;
+                    } else {
+                        if (OresomeNotes.getInstance().getServer().getPlayer(args[0]) != null) {
+                            ListIterator<String> read = Utility.iterRead(OresomeNotes.getInstance().getServer().getPlayer(args[0]));
+                            sender.sendMessage(ChatColor.GREEN + "=====Notes for " + ChatColor.RED + args[0] + ChatColor.GREEN + "======");
+                            while (read.hasNext()) {
+                                String in = read.next();
+                                if (!in.equals("dataManage--noedit")) {
+                                    sender.sendMessage(ChatColor.BLUE + Utility.readFromConfig(in));
+                                }
+                            }
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
+                        }
                     }
 
                 } else {
