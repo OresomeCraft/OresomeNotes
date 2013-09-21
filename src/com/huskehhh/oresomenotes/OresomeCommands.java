72f895c82268e869f2ac184614cb5bffc57d465b
psgs <fpigerre@gmail.com>
2013-09-21 11:05:59 +1000

    Improve notes' responses

--- src/com/huskehhh/oresomenotes/OresomeCommands.java
+++ src/com/huskehhh/oresomenotes/OresomeCommands.java
@@ -11,31 +11,36 @@ public class OresomeCommands implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (cmd.getName().equalsIgnoreCase("notes")) {
-            if (sender.hasPermission("oresomenotes.staff")) {
-                if (args.length <= 16 && args.length >= 0) {
+            if (sender.hasPermission("oresomenotes.staff") || sender.isOp()) {
+                if (args.length < 16 && args.length > 0) {
                     String query = args[0];
 
                     if (query.equalsIgnoreCase("add")) {
-                        String perform = args[1];
-                        String extra = Utility.argBuilding(args);
-                        if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
-                            Utility.addNote(OresomeNotes.getInstance().getServer().getPlayer(perform), Utility.prepareForConfig(extra));
+                        if (args[2] != null) {
+                            String perform = args[1];
+                            String extra = Utility.argBuilding(args);
+                            if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
+                                Utility.addNote(OresomeNotes.getInstance().getServer().getPlayer(perform), Utility.prepareForConfig(extra));
+                            } else {
+                                sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
+                            }
+                            sender.sendMessage(ChatColor.GREEN + "Successfully added note; " + extra + " to " + ChatColor.RED + perform + ChatColor.GREEN + "'s notes!");
                         } else {
-                            sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
+                            sender.sendMessage(ChatColor.RED + "You must enter a note to add!");
                         }
-                        sender.sendMessage(ChatColor.GREEN + "Successfully added note; " + extra + " to " + ChatColor.RED + perform + ChatColor.GREEN + "'s notes!");
 
                     } else if (query.equalsIgnoreCase("remove")) {
                         String perform = args[1];
-                        if (Integer.valueOf((args[2])) != null) {
-                            int entry = Integer.parseInt((args[2]));
+                        if (Integer.valueOf(args[2]) != null) {
+                            int entry = Integer.parseInt(args[2]);
                             if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
                                 Utility.removeNote(OresomeNotes.getInstance().getServer().getPlayer(perform), entry);
+                                sender.sendMessage(ChatColor.RED + "Note removed!");
                             } else {
                                 sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
                             }
                         } else {
-                            sender.sendMessage(ChatColor.RED + "Third paramater must be a number.");
+                            sender.sendMessage(ChatColor.RED + "Third parameter must be a number.");
                         }
                         return true;
 
@@ -43,7 +48,7 @@ public class OresomeCommands implements CommandExecutor {
                         String perform = args[1];
                         if (OresomeNotes.getInstance().getServer().getPlayer(perform) != null) {
                             ListIterator<String> read = Utility.iterRead(OresomeNotes.getInstance().getServer().getPlayer(perform));
-                            sender.sendMessage(ChatColor.GREEN + "=====Notes for " + ChatColor.RED + perform + ChatColor.GREEN + "======");
+                            sender.sendMessage(ChatColor.GREEN + "===== Notes for " + ChatColor.RED + perform + ChatColor.GREEN + " ======");
                             while (read.hasNext()) {
                                 String in = read.next();
                                 if (!in.equals("dataManage--noedit")) {
@@ -54,10 +59,11 @@ public class OresomeCommands implements CommandExecutor {
                             sender.sendMessage(ChatColor.RED + "You must enter a valid player name.");
                         }
                         return true;
+
                     } else {
                         if (OresomeNotes.getInstance().getServer().getPlayer(args[0]) != null) {
                             ListIterator<String> read = Utility.iterRead(OresomeNotes.getInstance().getServer().getPlayer(args[0]));
-                            sender.sendMessage(ChatColor.GREEN + "=====Notes for " + ChatColor.RED + args[0] + ChatColor.GREEN + "======");
+                            sender.sendMessage(ChatColor.GREEN + "===== Notes for " + ChatColor.RED + args[0] + ChatColor.GREEN + " ======");
                             while (read.hasNext()) {
                                 String in = read.next();
                                 if (!in.equals("dataManage--noedit")) {
@@ -71,6 +77,13 @@ public class OresomeCommands implements CommandExecutor {
 
                 } else {
                     sender.sendMessage(ChatColor.RED + "Amount of paramaters is wrong");
+                }
+                if (args.length == 0) {
+                    sender.sendMessage(ChatColor.GREEN + "===== OresomeNotes ====="); // 24 chars
+                    sender.sendMessage(ChatColor.RED + "/notes add (Player) (Note) " + ChatColor.GREEN + "Adds a note to a player");
+                    sender.sendMessage(ChatColor.RED + "/notes remove (Player) (Note Number) " + ChatColor.GREEN + "Removes a note");
+                    sender.sendMessage(ChatColor.RED + "/notes read (Player) " + ChatColor.GREEN + "Displays a player's notes");
+                    sender.sendMessage(ChatColor.GREEN + "Players need to be online for commands to work!");
                 }
             } else {
                 sender.sendMessage(ChatColor.RED + "No permission for this.");
