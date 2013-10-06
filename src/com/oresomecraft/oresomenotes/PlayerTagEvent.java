9c04250fcc572b341f0feadb953d203a9eb68645
R3creat3 <reicymc@gmail.com>
2013-10-06 13:57:51 +1100

    Fix all errors and bugs, improve code quality

---
+++ src/com/oresomecraft/oresomenotes/PlayerTagEvent.java
@@ -0,0 +1,37 @@
+package com.oresomecraft.oresomenotes;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public final class PlayerTagEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private String target;
+    private String tag;
+    private String tagger;
+
+    public PlayerTagEvent(String target, String tag, String tagger) {
+        this.target = target;
+        this.tag = tag;
+        this.tagger = tag;
+    }
+
+    public String getTagged() {
+        return target;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getTagger() {
+        return tagger;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
